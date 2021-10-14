58f523800dd67fdcae23b1e77d67fd8684a3779d
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-10-14 18:48:50 +0900

    debug

--- iris/shard/mount-disks.sh
+++ iris/shard/mount-disks.sh
@@ -1,8 +1,56 @@
 #!/bin/bash
 
-mkfs -t xfs /dev/sda
-mkfs -t xfs /dev/sdb
-mkfs -t xfs /dev/sdc
+is_partitioned() {
+    OUTPUT=$(partx -s ${1} 2>&1)
+    egrep "partition table does not contains usable partitions|failed to read partition table" <<< "${OUTPUT}" >/dev/null 2>&1
+    if [ ${?} -eq 0 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+has_filesystem() {
+    DEVICE=${1}
+    OUTPUT=$(file -L -s ${DEVICE})
+    grep filesystem <<< "${OUTPUT}" > /dev/null 2>&1
+    return ${?}
+}
+
+scan_for_new_disks() {
+    # Looks for unpartitioned disks
+    declare -a RET
+    DEVS=($(ls -1 /dev/sd*|egrep -v "[0-9]$"))
+    for DEV in "${DEVS[@]}";
+    do
+        # The disk will be considered a candidate for partitioning
+        # and formatting if it does not have a sd?1 entry or
+        # if it does have an sd?1 entry and does not contain a filesystem
+        is_partitioned "${DEV}"
+        if [ ${?} -eq 0 ];
+        then
+            has_filesystem "${DEV}1"
+            if [ ${?} -ne 0 ];
+            then
+                RET+=" ${DEV}"
+            fi
+        else
+            RET+=" ${DEV}"
+        fi
+    done
+    echo "${RET}"
+}
+
+# should find 3 disks
+DISKS=$(scan_for_new_disks)
+echo "DISKS=${DISKS}"
+
+for DISK in ${DISKS};
+do
+    echo "mkfs $DISK"
+    mkfs -t xfs $DISK
+done
+
 
 IRISROOT=/iristest
 mkdir ${IRISROOT}
@@ -11,14 +59,16 @@ mkdir ${IRISROOT}/wij
 mkdir ${IRISROOT}/journal1
 mkdir ${IRISROOT}/journal2
 
+DISKS=(${DISKS})
 MOUNT_OPTIONS=defaults,nofail
-read UUID FS_TYPE < <(blkid -u filesystem /dev/sda |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+read UUID FS_TYPE < <(blkid -u filesystem ${DISKS[0]} |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
 LINE="UUID=${UUID}\t${IRISROOT}/wij\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
 
-read UUID FS_TYPE < <(blkid -u filesystem /dev/sdb |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+read UUID FS_TYPE < <(blkid -u filesystem ${DISKS[1]} |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
 LINE="UUID=${UUID}\t${IRISROOT}/journal1\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
 
-read UUID FS_TYPE < <(blkid -u filesystem /dev/sdc |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+read UUID FS_TYPE < <(blkid -u filesystem ${DISKS[2]} |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
 LINE="UUID=${UUID}\t${IRISROOT}/db\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
 
 mount -a
+
