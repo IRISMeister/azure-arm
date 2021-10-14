b58889804bed42287ec6bc909d0eaf7d963f38fe
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-10-14 18:02:02 +0900

    debug

--- iris/shard/mount-disks.sh
+++ iris/shard/mount-disks.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+mkfs -t xfs /dev/sda
+mkfs -t xfs /dev/sdb
 mkfs -t xfs /dev/sdc
-mkfs -t xfs /dev/sdd
 
-mkdir /iristest
-mkdir /iristest/sys
-mkdir /iristest/db
-mkdir /iristest/jrnl
-mkdir /iristest/jrnl/pri
-mkdir /iristest/jrnl/alt
+IRISROOT=/iristest
+mkdir ${IRISROOT}
+mkdir ${IRISROOT}/db
+mkdir ${IRISROOT}/wij
+mkdir ${IRISROOT}/journal1
+mkdir ${IRISROOT}/journal2
 
-echo "/dev/sdc       /iristest/sys   xfs    defaults,nofail 0       2" >> /etc/fstab
-echo "/dev/sdd       /iristest/db   xfs    defaults,nofail 0       2" >> /etc/fstab
-echo "/dev/sde       /iristest/jrnl/pri   xfs    defaults,nofail 0       2" >> /etc/fstab
+MOUNT_OPTIONS=defaults,nofail
+read UUID FS_TYPE < <(blkid -u filesystem /dev/sda |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+LINE="UUID=${UUID}\t${IRISROOT}/wij\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
+
+read UUID FS_TYPE < <(blkid -u filesystem /dev/sdb |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+LINE="UUID=${UUID}\t${IRISROOT}/journal1\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
+
+read UUID FS_TYPE < <(blkid -u filesystem /dev/sdc |awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+LINE="UUID=${UUID}\t${IRISROOT}/db\t${FS_TYPE}\t${MOUNT_OPTIONS}\t0 2"; echo -e "${LINE}" >> /etc/fstab
 
 mount -a
