6d64d88c3dd654f0dc0c163faeb9897cc5872330
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2025-10-08 10:38:57 +0900

    wip

--- iris/windows/install.ps1
+++ iris/windows/install.ps1
@@ -1,3 +1,4 @@
+Param($SASTOKEN,$SECRETSLOCATION,$KITNAME)
 # IISインストール
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
@@ -6,3 +7,7 @@ $chromeInstaller = "$env:TEMP\\chrome_installer.exe"
 Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $chromeInstaller
 Start-Process -FilePath $chromeInstaller -Args "/silent /install" -Wait
 Remove-Item $chromeInstaller
+
+# IRISキットの取得
+$KITNAME="IRIS-2025.1.1.313.1-win_x64"+".exe"
+wget ${SECRETSLOCATION}/${KITNAME}?${SASTOKEN} -O $KITNAME
